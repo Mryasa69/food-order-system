5b4299ae8aad2f155093828785eee8e3c6732bac
Mryasa69 <yasaswickramarathna32@gmail.com>
2026-01-27 13:17:23 +0530

    update placeorder page

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './PlaceOrder.css'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
+
+    const { getTotalCartAmount } = useContext(StoreContext);
+
+
     return (
         <form className='place-order'>
             <div className="place-order-left">
@@ -23,7 +29,26 @@ const PlaceOrder = () => {
                 <input type="text" placeholder='Phone' />
             </div>
             <div className="place-order-right">
-                {/* Cart Totals section would go here */}
+                <div className="cart-total">
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>${getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>${2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <b>Total</b>
+                            <b>${getTotalCartAmount() + 2}</b>
+                        </div>
+                    </div>
+                    <button>PROCEED TO PAYMENT</button>
+                </div>
             </div>
         </form>
     )
